2d2caa24cba90082b5a63037c696048189939667
roberto-lumbreras <rlumbrerascanal@gmail.com>
2024-10-04 11:27:44 +0200

    Subido correctamente

--- api/src/main/java/com/example/Conexion.java
+++ api/src/main/java/com/example/Conexion.java
@@ -2,7 +2,6 @@ package com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Conexion {
@@ -10,44 +9,17 @@ public class Conexion {
     static String password="";
     static String url="jdbc:h2:mem:testdb";
 
-    public static Connection establecer(){
-        Connection cn = null;
-        try {
-            cn = DriverManager.getConnection(url, user, password);
-            PreparedStatement ps = cn.prepareStatement(
-            "CREATE TABLE IF NOT EXISTS usuarios ("
-            + "id INT PRIMARY KEY AUTO_INCREMENT,"
-            + "username VARCHAR(50) UNIQUE,"
-            + "password VARCHAR(100),"
-            + "rol VARCHAR(20)"
-            + ");");
-            ps.executeUpdate();
-            System.out.println("Tabla usuarios creada");
-            ps = cn.prepareStatement(
-            "CREATE TABLE IF NOT EXISTS solicitudes ("
-            + "id INT PRIMARY KEY AUTO_INCREMENT,"
-            + "id_usuario INT,"
-            + "fecha DATE,"
-            + "tema VARCHAR(100),"
-            + "descripcion TEXT,"
-            + "prioridad VARCHAR(20),"
-            + "FOREIGN KEY (id_usuario) REFERENCES usuarios(id)"
-            + ");");
-            ps.executeUpdate();
-            System.out.println("Tabla solicitudes creada");
-            ps = cn.prepareStatement(
-            "CREATE TABLE IF NOT EXISTS comentarios("
-            + "id INT PRIMARY KEY AUTO_INCREMENT,"
-            + "id_solicitud INT,"
-            + "comentario TEXT,"
-            + "FOREIGN KEY (id_solicitud) REFERENCES solicitudes(id)"
-            +");");
-            ps.executeUpdate();
-            System.out.println("Tabla comentarios creada");
-
-        } catch (SQLException e) {
-            System.out.println("Conexion fallida: "+e.getMessage());
-        }
+    public static Connection establecer() throws SQLException{
+        Connection cn = DriverManager.getConnection(url, user, password);
         return cn;
+    }
+
+    public static void iniciarServidor(){
+        try {
+            org.h2.tools.Server.createTcpServer().start();
+            System.out.println("Servidor H2 iniciado en el puerto 8080.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
