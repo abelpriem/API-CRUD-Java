959ab74ef4b8728cad557a3c01fa3fff835049a6
roberto-lumbreras <rlumbrerascanal@gmail.com>
2024-10-09 15:48:35 +0200

    Se ha cambiado la funcionalidad de la clase App.java ahora implmenta dos metodos estaticos que desplegaran un menu de consola y retornan un valor int en funcion del input del usuario

--- api/src/main/java/com/example/App.java
+++ api/src/main/java/com/example/App.java
@@ -9,11 +9,15 @@ import java.util.Scanner;
  */
 public class App 
 {
+/*
+
+    static String input;
     public static void main( String[] args )
     {   
         Object[]lista;
         String input;
         boolean exit=false;
+        int rol;
         Conexion.iniciarServidor();
         try{
         CrearTabla.usuarios();
@@ -21,6 +25,17 @@ public class App
         }catch(SQLException e){
             e.printStackTrace();
         }
+        System.out.println("Introducir rol");
+        System.out.println("1. Usuario | 2. Admin");
+        rol = new Scanner(System.in).nextInt();
+        if(rol == 1){
+            menuUsuario();
+        }else if(rol == 2){
+            menuAdmin();
+        }else{
+            System.out.println("Input no valido");
+        }
+
         while(!exit){
             System.out.println("Seleccionar opcion");
             System.out.println("1. Crear nueva solicitud");
@@ -84,6 +99,66 @@ public class App
                 default:
                     System.out.println("Input no valido");
             }
+        }
+    }
+
+    */
+
+    public static int menuAdmin() {
+        String input;
+        System.out.println("Seleccionar opcion");
+        System.out.println("1. Crear nueva solicitud");
+        System.out.println("2. Ver todas las solicitudes");
+        System.out.println("3. Modificar una solicitud");
+        System.out.println("4. Ver todas las solicitudes ordenado por fecha");
+        System.out.println("5. Ver si una solicitud esta en curso");
+        System.out.println("6. Marcar solicitud como pendiente");
+        System.out.println("7. Marcar solicitud como en curso");
+        System.out.println("8. Marcar solicitud como finalizada");
+        System.out.println("9. Eliminar solicitud finalizada");
+        input = new Scanner(System.in).next();
+        switch(input){
+            case "1":
+            return 1;
+            case "2":
+            return 2;
+            case "3":
+            return 3;
+            case "4":
+            return 4;
+            case "5":
+            return 5;
+            case "6":
+            return 6;
+            case "7":
+            return 7;
+            case "8":
+            return 8;
+            case "9":
+            return 9;
+            default:
+            System.out.println("Input no valido");
+            return 0;
+        }
+    }
+
+    public static int menuUsuario() {
+        String input;
+        System.out.println("Seleccionar opcion");
+        System.out.println("1. Crear nueva solicitud");
+        System.out.println("2. Ver todas las solicitudes");
+        System.out.println("3. Modificar una solicitud");
+        input = new Scanner(System.in).next();
+        switch(input){
+            case "1":
+            return 1;
+            case "2":
+            return 2;
+            case "3":
+            return 3;
+            default:
+            System.out.println("Input no valido");
+            return 0;
         }
     }
 }
